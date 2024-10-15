bd2fcf78e85fc1f874b48714f92b70e13c8c0426
Agustín Friadenrich <a.friadenrich@sistemas-utnfra.com.ar>
2024-10-15 18:14:41 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/weather/:city', (req, res) => {
+    let n = Math.floor(Math.random() * 6);
+    let desc = "";
+    
+    switch(n){
+        case 0:
+            desc = "Rainy";
+            break;
+        case 1:
+            desc = "Ligh rain";
+            break;
+        case 2: 
+            desc = "Super cloudy";
+            break;
+        case 3: 
+            desc = "Cloudy";
+            break;
+            default: "Sunny"
+    }
+
+    const response = {
+        "temperature": Math.floor(Math.random() * 35 - 5) + " °C",
+        "wind": Math.floor(Math.random() * 12) + " km/h",
+        "description": desc,
+        "forecast": [
+          {
+            "day": "1",
+            "temperature": Math.floor(Math.random() * 35 - 5) + " °C",            
+            "wind": Math.floor(Math.random() * 12) + " km/h"
+          },
+          {
+            "day": "2",
+            "temperature": Math.floor(Math.random() * 35 - 5) + " °C",
+            "wind": Math.floor(Math.random() * 12) + " km/h"
+          },
+          {
+            "day": "3",
+            "temperature": Math.floor(Math.random() * 35 -5) + " °C",
+            "wind": Math.floor(Math.random() * 12) + " km/h"
+          }
+        ]
+      }
+
+  res.send(response);  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
